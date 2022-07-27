008618a6224e52157124177330ad9d35a8008042
sainath goud <saisainath777@gmail.com>
2022-07-27 14:51:14 +0530

    added continuous increment or decrement by holding the btn feature

--- Counter App/script.js
+++ Counter App/script.js
@@ -1,3 +1,5 @@
+// increment or decrement by clicking the button
+
 btndown.addEventListener("click", valueD);
 btnup.addEventListener("click", valueU);
 
@@ -11,3 +13,45 @@ function valueU() {
   let n = parseInt(span.textContent);
   span.textContent = n + 1;
 }
+
+// continuos increment or decrement by holding down the button
+
+const btn1 = document.querySelector('#btnup');
+const count = document.querySelector('#count');
+let value = 0;
+let timer;
+
+function continuosIncerment() {
+  count.innerHTML = ++value;
+  
+  timer = setTimeout(continuosIncerment, 200);
+}
+
+function timeoutClear() {
+  clearTimeout(timer);
+}
+
+btn1.addEventListener('mousedown', continuosIncerment);
+
+btn1.addEventListener('mouseup', timeoutClear);
+
+btn1.addEventListener('mouseleave', timeoutClear);
+
+const btn2 = document.querySelector('#btndown');
+let timer2;
+
+function continuosdecerment() {
+  count.innerHTML = --value;
+  
+  timer2 = setTimeout(continuosdecerment, 200);
+}
+
+function timeoutClear2() {
+  clearTimeout(timer2);
+}
+
+btn2.addEventListener('mousedown', continuosdecerment);
+
+btn2.addEventListener('mouseup', timeoutClear2);
+
+btn2.addEventListener('mouseleave', timeoutClear2);
