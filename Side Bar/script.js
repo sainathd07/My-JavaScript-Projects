1e799e7b2c1c742a27811edf04c83b824c69707d
sainath goud <saisainath777@gmail.com>
2022-08-04 18:22:48 +0530

    Side Bar Project Completed

---
+++ Side Bar/script.js
@@ -0,0 +1,11 @@
+document.getElementById('cross-icon').addEventListener('click', none)
+
+function none() {
+document.getElementById('card').style.display = "none"
+}
+
+document.getElementById('menu-icon').addEventListener('click', show)
+
+function show() {
+  document.getElementById('card').style.display = null;
+}
